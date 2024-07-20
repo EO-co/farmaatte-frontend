f9a6a1c2688bad0b1667f0d5a6936e189e78dfb4
Emil Trangeled <emilbfh@hotmail.com>
2024-07-20 11:25:16 +0200

    Added newer dice package

--- app/games/dice/page.tsx
+++ app/games/dice/page.tsx
@@ -1,14 +1,39 @@
 "use client";
 import TopNavbar from "@/app/components/topNavbar";
-import Dice from "react-dice-roll";
+import { useRef } from "react";
+import ReactDice, { ReactDiceRef } from 'react-dice-complete' // Docs: https://github.com/AdamTyler/react-dice-complete
+
 
 export default function () {
+  const reactDice = useRef<ReactDiceRef>(null)
+
+  const rollDone = (totalValue: number, values: number[]) => {
+    console.log('individual die values array:', values)
+    console.log('total dice value:', totalValue)
+  }
+
+  const rollAll = () => {
+    reactDice.current?.rollAll()
+  }
+
   return (
     <>
       <div>{TopNavbar("Terning", "/games")}</div>
       <div className=" flex items-center justify-center py-10">
         <div className="">
-          <Dice size={100} />
+        <ReactDice
+          numDice={2}
+          ref={reactDice}
+          rollDone={rollDone}
+          faceColor="#F9F0DF"
+          dotColor="#070506"
+          outline={true}
+          dieSize={90}
+          margin={30}
+        />
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => rollAll()}>
+          Roll all
+        </button>
         </div>
       </div>
     </>
