6fdd4758626550e37749a2b60f2ba0b7072059fd
Fiedler12 <o.lyngholm@gmail.com>
2024-07-31 17:10:10 +0200

    Small change

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -18,6 +18,7 @@ export default function Login() {
   const router = useRouter();
 
   const submit = async () => {
+    setFailed(false);
     setLoading(true);
 
     const axiosInstance = axios.create({
